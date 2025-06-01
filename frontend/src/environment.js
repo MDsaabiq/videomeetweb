1b44be51f11d0681eaab11919efa6974e0e3d2cc
MDsaabiq <saabiqsaadiq55@gmail.com>
2025-06-01 15:47:03 +0530

    first commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,7 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://video-meet-backend-0wga.onrender.com" :
 
     "http://localhost:8000"
-
 
 export default server;
